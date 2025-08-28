05bac2a97487599c45e8eb01bc8fd37d3e734950
Mithun Gowda B <mithungowda.b7411@gmail.com>
2025-08-28 09:07:01 +0530

    Update update-script.py
            Removed the complex, multi-step commit logic from the script.
            Reason: With the workflows now using relative paths, the script no longer needs to create multiple commits to update action and workflow references. I have simplified it to only update the version number in the action.yml files and README.md in a single commit, and I've updated the help text to match.

--- update-script.py
+++ update-script.py
@@ -43,11 +43,10 @@ def print_help():
 Performs a series of git merges to update all references of the previous version to the specified tag of osv-scanner. This script expects upstream remote to be named `upstream`
 1. Fetch upstream main branch
 2. Create new branch on the most recent version tag (the last release commit)
-3. Update references to the old osv-scanner tag to the new tag, and make the first commit
-4. Update references to the old .github/workflows/osv-scanner-reusable.yml version to the newly made commit in the last step. Make the second commit.
-5. Finally update the unified workflow to point to the commit made in step 4, perform the third commit.
+3. Update references to the old osv-scanner tag to the new tag.
+4. Commit the changes.
 
-After this script is complete, push the new branch and create a PR. This PR must be merged via a normal git merge commit, NOT a squash commit.
+After this script is complete, push the new branch and create a PR.
 Then create the new release tag on this merged PR commit.''')
 
 
@@ -84,38 +83,5 @@ cmd([
     f'"Update actions to use {target_tag} osv-scanner image"'
 ])
 
-first_commit_hash = cmd(['git', 'rev-parse', 'HEAD'])
-print('First commit hash: ' + first_commit_hash)
-
-find_and_replace_regex_in_file(
-    '.github/workflows/osv-scanner-reusable.yml',
-    'uses: google/osv-scanner-action/osv-(.*?)-action@.*? # .*',
-    f'uses: google/osv-scanner-action/osv-\\1-action@{first_commit_hash} # {target_tag}'
-)
-
-find_and_replace_regex_in_file(
-    '.github/workflows/osv-scanner-reusable-pr.yml',
-    'uses: google/osv-scanner-action/osv-(.*?)-action@.*? # .*',
-    f'uses: google/osv-scanner-action/osv-\\1-action@{first_commit_hash} # {target_tag}'
-)
-
-cmd([
-    'git', 'commit', '-a', '-m',
-    f'Update reusable workflows to point to {target_tag} actions'
-])
-
-second_commit_hash = cmd(['git', 'rev-parse', 'HEAD'])
-print('Second commit hash: ' + second_commit_hash)
-
-find_and_replace_regex_in_file(
-    '.github/workflows/osv-scanner-unified-workflow.yml',
-    'uses: "google/osv-scanner-action/\\.github/workflows/osv-scanner-reusable(.*?)@.*?" # .*',
-    f'uses: "google/osv-scanner-action/.github/workflows/osv-scanner-reusable\\1@{second_commit_hash}" # {target_tag}'
-)
-
-cmd([
-    'git', 'commit', '-a', '-m',
-    f'Update unified workflow example to point to {target_tag} reusable workflows'
-])
-
 print('Success!')
+  
